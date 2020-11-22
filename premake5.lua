e96b4c76981a6f1ba6df8886e687e60014e4fcdc
Nellie <nelliostankova04@gmail.com>
2020-11-22 19:49:38 +0300

    Working with submodule

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "on"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -20,7 +21,6 @@ project "GLFW"
 
     filter "system:windows"
             systemversion "latest"
-            staticruntime "On"
 
             files
             {
@@ -44,7 +44,7 @@ project "GLFW"
         filter "configurations:Debug"
             runtime "Debug"
             symbols "on"
-    
+        
         filter "configurations:Release"
             runtime "Release"
             optimize "on"
